c5911d3bce9918707656c092d20f80748a59f6a3
nanoyaki <144328493+nanoyaki@users.noreply.github.com>
2024-10-06 00:36:43 +0200

    update monado api version

--- src/lib.rs
+++ src/lib.rs
@@ -25,7 +25,7 @@ use sys::MndRootPtr;
 use sys::MonadoApi;
 
 fn crate_api_version() -> VersionReq {
-	VersionReq::parse("^1.3.0").unwrap()
+	VersionReq::parse("^1.4.0").unwrap()
 }
 fn get_api_version(api: &Container<MonadoApi>) -> Version {
 	let mut major = 0;
